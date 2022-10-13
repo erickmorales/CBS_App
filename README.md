ebd29dabf8ae7dd81a89bbe03a1119f9b8b4bbad
erickmorales <ebenitez.morales@hotmail.com>
2022-10-13 02:15:12 -0600

    update

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CBS_App
  
+This solution contain the first stage about the test for CBS and part of the stage 2.
+
+Details:
+The solution FormSendMessage, contain three projects:
+* WindowsFormsAppSendMessage
+* FormSendMessageService
+* apiTwilio
+
+- The file demoAppCBS.mkv is the video showing a fast review about the code for the Stage 1.
+
+- The file messageWUP.bak is the backup file to make the restore for the SQL Data Base. This was done with the SQL Server Management Studio	15.0.18384.0
+
+- Also, this repository contain, the code for the backend part about the first point to the stage 2 (Send sms from twilio api to other number. This part work fine but only backend is ready).
---
+++ apiTwilio/Controllers/SmsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Mvc;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.TwiML;
+using Twilio.Types;
+
+
+
+namespace apiTwilio.Controllers
+{
+    public class SmsController : TwilioController
+    {
+        
+            public ActionResult SendSms()
+            {
+                string accountSid = ConfigurationManager.AppSettings["TwilioAccountSid"];
+                string authToken = Environment.GetEnvironmentVariable("TwilioAuthToken");
+                Twilio.TwilioClient.Init("AC146f11bb29f2b5ed200dfb899eb0b022", "eb776d4a1f410bd7813d357393e423e0");
+
+                var to = new PhoneNumber(ConfigurationManager.AppSettings["MyPhoneNumber"]);
+                var from = new PhoneNumber("+13608002455");
+
+                
+            var message = MessageResource.Create(
+                    to: new Twilio.Types.PhoneNumber("+50375655714"),
+                    from: new Twilio.Types.PhoneNumber("+13608002455"),
+                    body: "Hello, thanks for check my code");
+
+                
+            
+            return Content(message.Sid);
+        }
+
+     /*
+        
+        public ActionResult ReceiveSms()
+        {
+            var response = new MessagingResponse();
+            response.Message("The Robots are coming! Head for the hills!");
+
+            return TwiML(response);
+        }
+        */
+
+        
+     [HttpPost]
+     public TwiMLResult Index(SmsRequest incomingMessage)
+     {
+         var messagingResponse = new MessagingResponse();
+         messagingResponse.Message("The Robots are coming! Head for the hills!" + incomingMessage.Body);
+
+         return TwiML(messagingResponse);
+     }
+     
+
+    }
+}
